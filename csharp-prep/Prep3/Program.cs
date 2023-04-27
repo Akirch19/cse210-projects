60023b6576fbc757dd7bd589b87cc4db09596f2e
Austin Kirchner <austin.kirchner76@gmail.com>
2023-04-26 21:14:07 -0700

    prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicnumber = int.Parse(Console.ReadLine());
+        int number = -1;
+
+        while (number != magicnumber)
+        {
+            Console.Write("What is your guess? ");
+            number = int.Parse(Console.ReadLine());
+            if (magicnumber < number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicnumber > number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+
     }
 }
