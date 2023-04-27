def8c309989a980417b50008c1d89aa5b8b10bb4
Austin Kirchner <austin.kirchner76@gmail.com>
2023-04-26 22:00:09 -0700

    prep 4 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> numbers = new List<float>();
+        float userinput = -1;
+        while (userinput != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finsihed: ");
+
+            string userResponse = Console.ReadLine();
+            userinput = float.Parse(userResponse);
+
+            if (userinput != 0)
+            {
+                numbers.Add(userinput);
+            }
+        }
+
+        //part 1
+        float total = 0;
+        foreach (float number in numbers)
+        {
+            total += number;
+        }
+        Console.WriteLine($"The sum is: {total}");
+
+        //part2
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //part 3
+        float largest = numbers[0];
+        foreach (float number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest}");
+
+
+
+        float smallest = numbers[0];
+        foreach (float number in numbers)
+        {
+            if (number < smallest)
+            {
+                smallest = number;
+            }
+        }
+        Console.WriteLine($"The smallest number is: {smallest}");
+        
+    
+
     }
 }
